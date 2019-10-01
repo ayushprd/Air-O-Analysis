3be4a8ed0ec73ba801ccac154f1c277d74a58925
Ayush Prasad <ayush.prd@gmail.com>
2019-10-01 06:44:02 +0530

    added form

--- lookup/views.py
+++ lookup/views.py
@@ -4,12 +4,18 @@ import requests
 # Create your views here.
 def home(request):
 
-    api_request = requests.get("http://www.airnowapi.org/aq/observation/zipCode/current/?format=application/json&zipCode=20002&distance=25&API_KEY=80255E1C-5395-4191-8F32-BE148707DBE0")
+    if request.method == "POST":
+        zipcode = request.POST['ticker']
+        return render(request, 'home.html', {'zipcode':zipcode})
 
-    try:
-        api = json.loads(api_request.content)
-    except Exception as e:
-        api = "Error"
+    else:
+        api_request = requests.get("http://www.airnowapi.org/aq/observation/zipCode/current/?format=application/json&zipCode=20002&distance=25&API_KEY=80255E1C-5395-4191-8F32-BE148707DBE0")
+
+        try:
+            api = json.loads(api_request.content)
+        except Exception as e:
+             api = "Error"
+    
 
     return render(request, 'home.html', {'api':api})
 
