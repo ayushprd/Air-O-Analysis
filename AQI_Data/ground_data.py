db9e6aa238beda896bda2f8e8319bcfd882bbb32
Ayush Prasad <ayush.prd@gmail.com>
2019-10-20 02:04:50 +0530

    added script for fetching ground data

---
+++ AQI_Data/ground_data.py
@@ -0,0 +1,8 @@
+import requests
+import urllib.request
+
+location = 'Delhi' #the first letter of the location has to be capitalised
+
+url = ('https://api.openaq.org/v1/measurements?city={0}'.format(location))
+
+urllib.request.urlretrieve(url, '/home/ayush/Desktop/python.csv') #the system path where the csv would be saved
